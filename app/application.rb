61558bc579f2784568ddfd671738fdc670d73e92
Dan M <arconk@gmail.com>
2017-04-22 21:48:08 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,27 @@
 class Application
+  @@count=-0
+  @@n=20
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    num_1 = Kernel.rand(1..@@n)
+    num_2 = Kernel.rand(1..@@n)
+    num_3 = Kernel.rand(1..@@n)
+    resp.write "#{num_1}...#{num_2}...#{num_3}\n"
+
+    if num_1==num_2 && num_2==num_3
+      resp.write "You Win"
+      @@count=0
+      @@n=20
+    else
+
+      resp.write "You Lose\n\n\n1-#{@@n}\n\n\n\n#{@@count}"
+      @@count+=1
+      if @@count>=10
+        @@n-=1
+        @@count=0
+      end
+    end
     resp.finish
   end
 
